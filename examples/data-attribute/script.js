f748fd3f98297e998eb6b66a82c37e1a635e5ccd
Robbert Wijtman <robbert@xinxmedia.com>
2024-03-18 21:23:11 -0700

    some stuff

--- examples/data-attribute/script.js
+++ examples/data-attribute/script.js
@@ -1,5 +1,3 @@
-document.querySelector("div").addEventListener("click", function(event) {
-    var div = event.target;
-
-    alert(div.dataset.number);
+document.querySelector("button").addEventListener("click", function(event) {
+     
 });
