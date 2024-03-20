26b795f68bfa41c08e0a47ebf636cd7b69313408
Robbert Wijtman <robbert@xinxmedia.com>
2024-03-19 18:44:56 -0700

    pre

---
+++ preclass/last_word_length.js
@@ -0,0 +1,43 @@
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal
+substring
+consisting of non-space characters only.
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of only English letters and spaces ' '.
+    There will be at least one word in s.
+*/
+
+function lastWordLength(s) {
+    var words = s.trim().split(" ");
+
+    var lastWord = words.at(-1);
+
+    return lastWord.length;
+}
+
+console.log(lastWordLength("Hello World"));
+console.log(lastWordLength("   fly me   to   the moon  "));
+console.log(lastWordLength("luffy is still joyboy"));
