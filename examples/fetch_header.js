9cd567a1336dd3ce4d91877a73d4444c55fc59e0
Robbert Wijtman <robbert@xinxmedia.com>
2024-04-01 18:15:50 -0700

    things

--- examples/fetch_header.js
+++ examples/fetch_header.js
@@ -1,8 +1,64 @@
-var url = "some url";
+var url = "https//something.com/api/user?token=sdfdsfdsfds";
 
-fetch(url, {
+const constant = 5;
+
+console.log(constant);
+
+if (true) {
+
+}
+
+while (true) {
+    break;
+}
+
+class blah {
+
+}
+
+new blah()
+
+for () {
+
+}
+
+function blah () {
+
+}
+
+blah(function () {
+
+})
+
+function MyClass() {}
+
+var ting = new Promise()
+var thing2 = new MyClass()
+
+const promise = fetch(url, {
+    method: 'POST',
     headers: {
         'content-type': 'application/json',
+        "Authentication": "Bearer sldfjdslfkdsjfldskfjdslk",
     },
-    body: 'some body',
+    body: JSON.stringify({
+        name: 'robbert',
+        username: 'robbert',
+        email: 'blah@blah.com',
+        age: 34,
+    }),
+}).then((result) => {
+    if (result.ok) {
+        return result.json();
+    }
+    throw new Error("No user data");
+}).then((data, data2) => {
+    // data is the result
+    console.log(data2);
+
+    return fetch(url);
+}).then((result2) => {
+
+}).catch((error) => {
+
 });
